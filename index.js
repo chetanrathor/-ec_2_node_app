04f57f3bb43667668fa21b2cdb37a17bfa12f4e6
chetan <chetan@gmail.com>
2023-12-17 23:38:05 +0530

    api2

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express()
 app.get('/api', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/api2', (req, res) => {
+  res.send('Hello World! from api 2')
+})
 
 app.listen(3333,()=>{
     console.log('Server is running on port dhgsd hdsjkhdskjhd 3333')
